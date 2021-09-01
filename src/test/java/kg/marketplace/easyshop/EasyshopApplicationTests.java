cf6bdb85af1a042328d25d093fb1bfc37a6e3b7a
NodirUmarov <mr.umarov.1995@gmail.com>
2021-09-01 10:17:18 +0600

    Initial commit

---
+++ src/test/java/kg/marketplace/easyshop/EasyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.marketplace.easyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
